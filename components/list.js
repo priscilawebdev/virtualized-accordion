a45a7dc0b60b12935136c09c8e17eaad88c9eff8
oliveirap <priscila.oliveira@semantic-web.com>
2018-07-13 11:35:46 +0200

    refactor: now div && new styles

--- components/list.js
+++ components/list.js
@@ -1,8 +1,16 @@
 import styled from 'preact-emotion'
 
-const List = styled('ul')`
-  width: 100%;
-  background-color: #fff;
+const List = styled('div')`
+  position: relative;
+  width: 800px;
+  height: 400px;
+  background-color: #f8f8f8;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow-x: hidden;
+  overflow-y: scroll;
 `
 
 export default List

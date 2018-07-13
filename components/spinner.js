1014a09cf926231d7969b8ee4fd269433ba1ef41
oliveirap <priscila.oliveira@semantic-web.com>
2018-07-13 11:30:20 +0200

    feat: add loading component

---
+++ components/spinner.js
@@ -0,0 +1,63 @@
+import styled from 'preact-emotion'
+
+const Wrapper = styled('div')(
+  {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  ({ centered = true }) => ({
+    centered,
+  })
+)
+
+const StyledSpinner = styled('svg')`
+  animation: rotate 2s linear infinite;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+
+  & .path {
+    stroke: #5652bf;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`
+
+const Spinner = props => (
+  <Wrapper {...props}>
+    <StyledSpinner viewBox="0 0 50 50">
+      <circle
+        className="path"
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        strokeWidth="4"
+      />
+    </StyledSpinner>
+  </Wrapper>
+)
+
+export default Spinner

69d8b3cd96e0a66879c9caac6c42fb5f9050b5e4
oliveirap <priscila.oliveira@semantic-web.com>
2018-07-19 09:06:14 +0200

    refactor: updated css

--- components/list/list.js
+++ components/list/list.js
@@ -5,12 +5,8 @@ const List = styled('div')`
   width: 800px;
   height: 400px;
   background-color: #f8f8f8;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   overflow-x: hidden;
   overflow-y: scroll;
-`
+}`
 
 export default List

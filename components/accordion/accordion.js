2c3c4cfd281d1ee35b1271237f18fb3fc0a7ca71
oliveirap <priscila.oliveira@semantic-web.com>
2018-07-23 13:32:15 +0200

    refactor: updated style

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -1,10 +1,14 @@
 import styled from 'preact-emotion'
 
 const Accordion = styled('div')`
-  border: 1px solid #ccc;
-  border-radius: 3px;
-  margin-bottom: 70px;
-  border-top: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 400px;
+  background-color: #f8f8f8;
+  overflow: hidden;
 `
 
 export default Accordion
